8f31801905fb8592d4d8321bfa99f239df4fae90
Duonganhdu2002 <Wydanhdu2002@gmail.com>
2024-09-15 19:09:51 +0700

    Home

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import renderer from 'react-test-renderer';
 
 import { ThemedText } from '../ThemedText';
+import { it, expect } from '@jest/globals';
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
   expect(tree).toMatchSnapshot();
 });
