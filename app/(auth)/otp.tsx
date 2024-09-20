e2b061d9234ac2cbc86b95322808b150fac046da
Nav <vunhatanh9597@gmail.com>
2024-09-20 21:33:37 +0700

    sua otp

--- app/(auth)/otp.tsx
+++ app/(auth)/otp.tsx
@@ -37,51 +37,50 @@ const Otp = () => {
 
   return (
     <SafeAreaView
-        className=" bg-white flex-1"
-        style={{
-          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        }}
-      >
-      <View className="flex justify-between bg-slate-200">
-        <Text className="text-2xl font-bold text-center mb-3">
-          Xác nhận Mã OTP
-        </Text>
-
-        <Text className="text-center text-base text-black mb-12">
-          Mã xác thực gồm 5 số đã được gửi đến Zalo hoặc số điện thoại
-          <TouchableOpacity
-                onPress={() => router.push("/home")}
-              >
-          <Text className="text-blue-500"> 09999999</Text>
-          </TouchableOpacity>
-        </Text>
-
-        <Text className="text-center text-base text-black mb-2">
-          Nhập mã để tiếp tục
-        </Text>
-
-        <View className="flex flex-row justify-center text-center px-2 mb-5">
-          {[...Array(5)].map((_, index) => (
-            <View
-              key={index}
-              className={`w-14 h-16 ${index < 4 ? "mr-4" : ""}`}
-            >
-              <TextInput
-                ref={(ref) => (inputs.current[index] = ref)}
-                className="w-full h-full text-center text-3xl leading-none items-center justify-center rounded-xl border border-gray-200 bg-white focus:bg-gray-100"
-                keyboardType="numeric"
-                maxLength={1}
-                value={values[index]}
-                onChangeText={(text) => handleChange(text, index)}
-                onKeyPress={(event) => handleKeyPress(event, index)}
-              />
-            </View>
-          ))}
+      className="bg-white flex-1"
+      style={{
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}>
+      
+      <View className="flex-1">
+        <View className="h-[5%] mt-10">
+          <Text className="text-2xl font-bold text-center">Xác nhận Mã OTP</Text>
         </View>
-        <Text className="text-center text-sm text-zinc-800">
-          Bạn không nhận được mã?{" "}
-          <Text className="text-slate-500">Gửi lại</Text>
-        </Text>
+
+        <View className="h-[6%] mb-2 ml-5 mr-5">
+          <Text className="text-center text-base text-black">
+            Mã xác thực gồm 5 số đã được gửi đến Zalo hoặc số điện thoại
+              <Text className="text-blue-500"> 09999999</Text>
+          </Text>
+        </View>
+
+        <View className="h-[20%] mt-3">
+          <Text className="text-center text-base text-black mb-2">
+            Nhập mã để tiếp tục
+          </Text>
+
+          <View className="h-[20%]flex flex-row justify-center text-center px-2 ">
+            {[...Array(5)].map((_, index) => (
+              <View key={index} className={`w-14 h-16 ${index < 4 ? "mr-4" : ""}`}>
+                <TextInput
+                  ref={(ref) => (inputs.current[index] = ref)}
+                  className="w-full h-full text-center text-3xl leading-none items-center justify-center rounded-xl border border-gray-400 bg-white focus:bg-gray-100"
+                  keyboardType="numeric"
+                  maxLength={1}
+                  value={values[index]}
+                  onChangeText={(text) => handleChange(text, index)}
+                  onKeyPress={(event) => handleKeyPress(event, index)}
+                />
+              </View>
+            ))}
+          </View>
+
+          <View className="h-[10%] mt-2">
+            <Text className="text-center text-sm text-zinc-800">
+              Bạn không nhận được mã? <Text className="text-slate-500">Gửi lại</Text>
+            </Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
